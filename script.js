20f833bea37b771d01f0028a9ab42313bbb47b61
Taj036 <121137573+Taj036@users.noreply.github.com>
2024-01-06 13:52:18 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function createTable() {
     //Write your code here
-  
+  let row = prompt('Input number of rows');
+  let col = prompt('Input number of columns');
+	createTable1(row , col)
+}
+function createTable1(row , col){
+	let table = document.getElementById('myTable');
+	let tablehtml = '';
+	for(let i = 0 ; i< row ; i++){
+		tablehtml += '<tr>' ;
+		for(let j = 0 ; j < col ; j++){
+			tablehtml += `<td>Row-${i} Column-${j}</td>`;
+		}
+		tablehtml += '</tr>'
+	}
+	table.innerHTML = tablehtml ;
 }
